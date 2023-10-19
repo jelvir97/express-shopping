81d32b27cd56db9683f2cf36d5749561b068117b
Juan Elvir <jelvir97@gmail.com>
2023-10-18 20:33:38 -0600

    error handling

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ app.use(express.json())
 
 app.use('/items',itemRoutes)
 
+app.use(function (req, res, next) {
+    const notFoundError = new ExpressError("Not Found", 404);
+    return next(notFoundError)
+})
+
+app.use((err,req,res,next)=>{
+    let message = err.message
+    let status = err.status ? err.status:500
+    return res.status(status).json({
+        error: {message, status}
+      });
+})
+
 app.listen(3000, ()=>{
     console.log('SERVER RUNNING ON PORT 3000')
 })
--- itemRoutes.js
+++ itemRoutes.js
@@ -1,42 +1,75 @@
 const express = require('express')
 const router = new express.Router()
 const items = require('./fakeDB')
+const ExpressError = require('./expressError')
 
 router.get('/', (req,res,next)=>{
-    res.json({items})
+    try{
+        if(items) return res.json({items})
+        throw new ExpressError('No items in fakeDB', 404)
+    }catch(err){
+        next(err)
+    }
+
 })
 
 router.post('/', (req,res,next)=>{
-    items.push(req.body.item)
-    res.json({added:req.body.item})
+    try{
+        if(req.body.item && req.body.item.name && req.body.item.price){
+            items.push(req.body.item)
+            return res.status(201).json({added:req.body.item})
+        }
+        throw new ExpressError('Invalid Request:Request body must have a JSON object named item with the keys of name and price',400)
+    }catch(err){
+        next(err)
+    }
+    
 })
 
 router.get('/:name', (req,res,next)=>{
-    console.log('Looking for items named' + req.params.name)
-    for(let i of items){
-        if(i.name === req.params.name){
-            return res.json(i)
+    try{
+        for(let i of items){
+            if(i.name === req.params.name){
+                return res.json(i)
+            }
         }
+        throw new ExpressError('Item not found', 404)
+    }catch(err){
+        next(err)
     }
 })
 
 router.patch('/:name',(req,res,next)=>{
-    for(let x in items){
-        if(items[x].name === req.params.name){
-            items[x] = req.body.item
-            return res.json({'updated': items[x]})
+    try{
+        if(!req.body.item || !req.body.item.name || !req.body.item.price){
+            throw new ExpressError('Invalid Request:Request body must have a JSON object named item with the keys of name and price',400)
         }
+        for(let x in items){
+            if(items[x].name === req.params.name){
+                items[x] = req.body.item
+                return res.json({'updated': items[x]})
+            }
+        }
+        throw new ExpressError('Item not found', 404)
+    }catch(err){
+        next(err)
     }
 })
 
 router.delete('/:name', (req,res,next)=>{
-    for(let x in items){
-        if(items[x].name === req.params.name){
-            items[x] = null
-            items.splice(x,1)
-            return res.json({'message': 'Deleted'})
+    try{
+        for(let x in items){
+            if(items[x].name === req.params.name){
+                items[x] = null
+                items.splice(x,1)
+                return res.json({'message': 'Deleted'})
+            }
         }
+        throw new ExpressError('Item not found', 404)
+    }catch(err){
+        next(err)
     }
+
 })
 
 module.exports = router 
